dc0650825d2260547f0759aeea7f94e307a11244
ALuthfiH <56031025+ALuthfiH@users.noreply.github.com>
2021-05-23 19:21:36 +0700

    Update soal2c.c

--- soal2/soal2c.c
+++ soal2/soal2c.c
@@ -1,84 +1,78 @@
-#include <stdlib.h>
-#include <unistd.h>
 #include <stdio.h>
-#include <string.h>
-#include <sys/wait.h>
+#include <stdlib.h>
 #include <sys/types.h>
-#include <errno.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-int main() 
-{ 
-    pid_t pid1, pid2, pid3;
-    
-	int fd1[2];
-	int fd2[2]; 
+int main() {
 
-	if (pipe(fd1)==-1) 
-	{ 
-		perror("bad pipe1");
-        exit(1);
-	} 
+    int pid;
+    int fd1[2];
+    int fd2[2];
 
-    pid1 = fork();
-    if(pid1 == -1) {
-        perror("fork1 error");
+    if (pipe(fd1) == -1) {
+        perror("bad pipe1");
         exit(1);
     }
-    else if(pid1 == 0) {
+
+    if ((pid = fork()) == -1) {
+        perror("bad fork1");
+        exit(1);
+    } 
+    else if (pid == 0) {
         dup2(fd1[1], 1);
+
         close(fd1[0]);
         close(fd1[1]);
 
-        char *argv1[] = {"ps", "aux", NULL};
-            execv("/bin/ps", argv1);
-
-        perror("ps aux execute doesn't work");
-        exit(1);
+        char *argv[] = {"ps", "aux", NULL};
+            execv("/bin/ps", argv);
+        perror("ps aux execute doesnt work");
+        _exit(1);
     }
 
-    if(pipe(fd2)==-1) {
+    if (pipe(fd2) == -1) {
         perror("bad pipe2");
         exit(1);
     }
 
-    pid2 = fork();
-    if(pid2 == -1) {
-        perror("fork2 error");
+    if ((pid = fork()) == -1) {
+        perror("bad fork2");
         exit(1);
-    }
-    else if(pid2 == 0) {
+    } 
+    else if (pid == 0) {
         dup2(fd1[0], 0);
         dup2(fd2[1], 1);
+
         close(fd1[0]);
         close(fd1[1]);
         close(fd2[0]);
         close(fd2[1]);
-
-        char *argv2[] = {"sort", "-nrk", "3,3", NULL};
-            execv("/bin/sort", argv2);
-
-        perror("sort execute doesn't work");
-        exit(1);
+ 
+        char *argv[] = {"sort", "-nrk", "3.3", NULL};
+            execv("/usr/bin/sort", argv);
+        perror("sort execute doesnt work");
+        _exit(1);
     }
 
     close(fd1[0]);
     close(fd1[1]);
 
-    pid3 = fork();
-    if (pid3 == -1) 
-    {
-        perror("fork3 error");
+    if ((pid = fork()) == -1) {
+        perror("bad fork3");
         exit(1);
     } 
-    else if (pid3 == 0) 
-    {
+    else if (pid == 0) {
         dup2(fd2[0], 0);
+
         close(fd2[0]);
         close(fd2[1]);
-        char *argv3[] = {"head", "-5", NULL};
-            execv("/bin/head", argv3);
-        perror("head execute doesn't work");
-        exit(1);
+
+        char *argv[] = {"head", "-5", NULL};
+            execv("/usr/bin/head", argv);
+
+        perror("head execute doesnt work");
+        _exit(1);
     }
-    exit(0);
+
 }
